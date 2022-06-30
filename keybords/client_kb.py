a11d24939b3a84a4bf58e80a35bffccf6c8bbed3
blackdrozd <Linch007@mail.ru>
2022-06-30 22:09:24 +1000

    Добавлена клавиатура

---
+++ keybords/client_kb.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+b1 = KeyboardButton('Поиск_по_названию')
+b2 = KeyboardButton('Поиск_по_описанию')
+b3 = KeyboardButton('Случайный')
+
+kb_client = ReplyKeyboardMarkup()
+kb_client.add(b1).add(b2).add(b3)
